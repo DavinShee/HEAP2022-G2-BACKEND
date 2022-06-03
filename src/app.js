e7bd24133d9779cbbd57ebc90b3d9c9dfa25b3c3
Justin Wang <ajustinwang@gmail.com>
2022-06-04 02:14:28 +0800

    added mongoose connection

--- src/app.js
+++ src/app.js
@@ -2,11 +2,16 @@ require('dotenv').config();
 const express = require('express');
 const moment = require('moment');
 const route = require('./routes/index');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use('/routes', route);
+app.use(express.json());
+mongoose.connect('mongodb://localhost:27017/HEAP', {
+    useNewUrlParser: true
+});
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
