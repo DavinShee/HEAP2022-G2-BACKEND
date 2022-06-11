c663654ec4f2c99cc8e81e902eaf41106d564214
Alethea Toh <aletheatohh@gmail.com>
2022-06-11 23:24:27 +0800

    Update index.js
    adding account route (for logging in and creation of account)

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const buyerRoute = require('./buyer/index');
-
+const accountRoute = require('./account/index');
+ 
 const router = express.Router();
-
+ 
+// basic structure: app.METHOD(PATH, HANDLER)
 router.use('/buyer', buyerRoute);
+router.use('/account', accountRoute);
 module.exports = router;
