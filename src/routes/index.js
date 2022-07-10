cbb695d225cc7f68d71903e8443c446868def8c6
Alethea Toh <aletheatohh@gmail.com>
2022-07-11 01:05:41 +0800

    added route for user

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const buyerRoute = require('./buyer/index');
 const accountRoute = require('./account/index');
 const downloadHistory = require('./downloadHistory/index');
+const userRoute = require('./user/index');
 
 const router = express.Router();
 
@@ -9,5 +10,6 @@ const router = express.Router();
 router.use('/buyer', buyerRoute);
 router.use('/account', accountRoute);
 router.use('/downloadHistory', downloadHistory);
+router.use('/user', userRoute);
 
 module.exports = router;
