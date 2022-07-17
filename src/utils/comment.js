75d1a72e1fc269247fcb968570b3eda69a4c605d
Alethea Toh <aletheatohh@gmail.com>
2022-07-17 23:59:01 +0800

    Updated comments function (one email can only comment on a post once)

--- src/utils/comment.js
+++ src/utils/comment.js
@@ -3,14 +3,26 @@ const notesModel = require('../models/notes');
 module.exports = {
     addComments: async (id, comment) => {
         try {
-            let doc = await notesModel.findOneAndUpdate(
-                { _id: id },
-                { $push: { comments: comment } },
-                {
-                    new: true
+            let userComments = await notesModel.findOne({ _id: id })
+            if (userComments.comments) {
+                if (userComments.comments.filter( e=> e.email === comment.email).length == 0) {
+                    let doc = await notesModel.findOneAndUpdate(
+                        { _id: id },
+                        { $push: { comments: comment } },
+                        {
+                            new: true
+                        }
+                    );
+                    return [undefined, doc];
                 }
-            );
-            return [undefined, doc];
+                error = "user has already uploaded comment on this post before";
+                console.error('Error adding comment to note', conditions, error);
+                return [error, null];
+            } else {
+                error = "Error getting note";
+                console.error('Error adding comment to note', conditions, error);
+                return [error, null];
+            }
         } catch (error) {
             console.error('Error adding comment to note', conditions, error);
             return [error, null];
