d785afcc48a5996f48735ced519592344fb23b2e
DavinShee <105922278+DavinShee@users.noreply.github.com>
2022-07-24 03:11:41 +0800

    increase payload limit

--- src/app.js
+++ src/app.js
@@ -10,8 +10,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use('/routes', route);
 app.use(express.json());
 
