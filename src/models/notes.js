b540b4c15a16f976ba98d5edc36d664b6fb7c0b7
Justin Wang <ajustinwang@gmail.com>
2022-07-24 02:51:28 +0800

    Update image type

--- src/models/notes.js
+++ src/models/notes.js
@@ -7,7 +7,7 @@ const noteSchema = new mongoose.Schema(
         description: { type: String, required: false },
         download: { type: Number, required: true },
         email: { type: String, required: true },
-        image: [{ type: Object, required: true }],
+        image: { type: String, required: true },
         modId: { type: String, required: true },
         price: { type: Number, required: false },
         profName: { type: String, required: true },
